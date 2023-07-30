6de33bc85162aa358bc0cb1480ff8bcf5964c5aa
matthew <matvey.zvancov10@gmail.com>
2023-07-30 21:07:38 +0300

    main function

--- BlogNN.py
+++ BlogNN.py
@@ -7,6 +7,10 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from text_transforms import get_features
 
+SAMPLES_NUM = 459
+TRAIN_SAMPLES = 380
+VALID_SAMPLES = 60
+
 
 class BlogClassifier(nn.Module):
     def __init__(self):
@@ -31,6 +35,7 @@ class BlogTextDataset(Dataset):
     def __init__(self, features, labels):
         self.features = features
         self.labels = labels
+        super().__init__()
 
     def __len__(self):
         return len(self.features)
@@ -52,11 +57,31 @@ def __define_label(author: str) -> int:
         raise ValueError(f"Not correct username from telegram: {author}")
 
 
-def _split_data(data, indices):
-    pass
+def train(model: nn.Module, train_dataloader, device,
+          loss_fn, optimizer: torch.optim.Optimizer,
+
+          verbose: bool = False,
+          n_epochs: int = 50):
+    model.train()
+    for epoch in range(1, n_epochs + 1):
+        for batch, (X, y) in enumerate(train_dataloader, start=1):
+            X, y = X.to(device), y.to(device)
+
+            prediction = model(X)
+            class_probabilities = torch.argmax(prediction, dim=1)
+            loss = loss_fn(class_probabilities, y)
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if verbose and batch % 10 == 0:
+                print(f"Loss {loss.item()} on epoch {epoch}")
+
 
 def main():
     random.seed(42)
+    np.random.seed(42)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f"Current computing device: {device}")
@@ -73,9 +98,33 @@ def main():
     authors = map(lambda el: el['author'], data)
 
     texts_features = get_features(texts, mode='train')
-    authors_labels = np.array(map(__define_label, authors), dtype=int)
+    authors_labels = np.array(list(map(__define_label, authors)), dtype=int)
 
-    dataset = BlogTextDataset(texts_features, )
+    indices = np.arange(SAMPLES_NUM)
+    np.random.shuffle(indices)
+    train_indices = indices[:TRAIN_SAMPLES]
+    valid_indices = indices[TRAIN_SAMPLES: TRAIN_SAMPLES + VALID_SAMPLES]
+    test_indices = indices[TRAIN_SAMPLES + VALID_SAMPLES:]
+
+    train_texts, train_labels = texts_features[train_indices], authors_labels[train_indices]
+    valid_texts, valid_labels = texts_features[valid_indices], authors_labels[valid_indices]
+    test_texts, test_labels = texts_features[test_indices], authors_labels[test_indices]
+
+    train_dataset = BlogTextDataset(train_texts, train_labels)
+    valid_dataset = BlogTextDataset(valid_texts, valid_labels)
+    test_dataset = BlogTextDataset(test_texts, test_labels)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=True)
+
+    loss_fn = nn.BCELoss()
+    optimizer = torch.optim.SGD(neural_network.parameters(),
+                                lr=0.01, momentum=0.004)  # SGD with momentum
+
+    train(neural_network, train_dataloader, device, loss_fn, optimizer, verbose=True)
+
+
 
 
 if __name__ == '__main__':
